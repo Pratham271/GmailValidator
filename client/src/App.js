cedc1635854e6633502b87b44e764cf0c4495209
Pratham_271 <pratham19csu221@ncuindia.edu>
2023-12-19 07:27:09 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main/Main";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
+
+function App() {
+	const user = localStorage.getItem("token");
+
+	return (
+		<Routes>
+			{user && <Route path="/" exact element={<Main />} />}
+			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+		</Routes>
+	);
+}
+
+export default App;
